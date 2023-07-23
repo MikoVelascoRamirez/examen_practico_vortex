0b8c3958e41fc4c338cb71866240846d25ec4fb6
Yair <yairvelascor05@gmail.com>
2023-07-23 01:03:35 -0600

    7. feat: including router and routes components

--- frontend/editorial_chavez_demo_app/src/main.jsx
+++ frontend/editorial_chavez_demo_app/src/main.jsx
@@ -1,7 +1,25 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Login from './login.jsx'
+import Catalog from './catalog.jsx';
+import BookComments from './BookComments.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <Login/>
+  },
+  {
+    path: '/catalog',
+    element: <Catalog/>
+  },
+  {
+    path: '/catalog/book/:isbnBook',
+    element: <BookComments />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render( <App /> );
+root.render( <RouterProvider router={router}/> );
