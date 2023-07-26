6825d4b80f89c327fe5a3a5a100700efccb51097
Yair <yairvelascor05@gmail.com>
2023-07-25 22:48:12 -0600

    26. styles: styling select component

--- frontend/editorial_chavez_demo_app/src/components/FilterBy.jsx
+++ frontend/editorial_chavez_demo_app/src/components/FilterBy.jsx
@@ -1,6 +1,8 @@
 export default function FilterBy(){
   return (
     <form action="" className="d-flex justify-content-evenly col-5">
+      <label htmlFor="filter" className="d-flex align-items-center justify-content-center col-3 text-white">Buscar por:</label>
+      <select name="filter" className="col-6 ps-2">
         <option selected disabled>Seleccione una opción</option>
         <option value="">Género</option>
         <option value="">Serie</option>
