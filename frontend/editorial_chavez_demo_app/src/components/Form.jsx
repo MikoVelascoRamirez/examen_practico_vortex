cf8e388608e3779b818983d798d423224eb011a7
Yair <yairvelascor05@gmail.com>
2023-07-25 20:45:26 -0600

    18. styles: making form's fieldset responsive

--- frontend/editorial_chavez_demo_app/src/components/Form.jsx
+++ frontend/editorial_chavez_demo_app/src/components/Form.jsx
@@ -3,6 +3,8 @@ import InputControl from './InputControl'
 export default function Form (){
   return (
     <form action="" className="container d-flex flex-column h-75 py-3 bg-secondary bg-opacity-25 rounded-4" id="form-login">
+        <h1 className='display-5 text-light'>Inicio de sesión</h1>
+        <fieldset className="mx-auto d-flex flex-column align-items-center py-4">
           <InputControl field={"User"} type={"text"}></InputControl>
           <InputControl field={"Password"} type={"password"}></InputControl>
         </fieldset>
