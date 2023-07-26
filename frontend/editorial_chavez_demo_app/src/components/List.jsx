abeaef5804dd6766fbb99f4adf53e2cb34fe84e7
Yair <yairvelascor05@gmail.com>
2023-07-26 17:03:58 -0600

    32. styles: styling container of books list

--- frontend/editorial_chavez_demo_app/src/components/List.jsx
+++ frontend/editorial_chavez_demo_app/src/components/List.jsx
@@ -1,8 +1,10 @@
 import Book from "./Book"
+import "../App.css";
 
 export default function List(){
   return (
-    <div>
+    <div id="list-container">
+      <div className="d-flex mx-auto flex-wrap justify-content-around align-items-end h-100 overflow-auto">
         <Book comments={2}>Libro 1</Book>
         <Book>Libro 2</Book>
         <Book comments={6}>Libro 3</Book>
@@ -13,6 +15,7 @@ export default function List(){
         <Book>Libro 8</Book>
         <Book comments={1}>Libro 9</Book>
         <Book>Libro 10</Book>
+      </div>
     </div>
   )
 }
