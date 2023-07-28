a90609819a11f2e9f714273e783c2c77f7dff754
Yair <yairvelascor05@gmail.com>
2023-07-27 22:45:43 -0600

    40. styles: making book component responsive.

--- frontend/editorial_chavez_demo_app/src/components/Book.jsx
+++ frontend/editorial_chavez_demo_app/src/components/Book.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export default function Book({children, comments}){
   return (
-    <div className='border col-2 h-50 my-4 mx-2 d-flex justify-content-center align-items-center text-white position-relative'>
+    <div className='bg-primary col-lg-2 col-md-3 col-sm-4 mx-sm-2 d-flex justify-content-center h-50 my-2 align-items-center text-white position-relative book mx-auto'>
         {
             !comments || <small className='position-absolute top-0 start-100 translate-middle badge bg-success rounded-circle p-2'>{comments}</small>
         }
