464d4509a60e8ce6b2cdcde90197dff7d5b91735
Yair <yairvelascor05@gmail.com>
2023-07-27 22:41:46 -0600

    39. styles: adding padding to container and deleting first wrapper.

--- frontend/editorial_chavez_demo_app/src/components/List.jsx
+++ frontend/editorial_chavez_demo_app/src/components/List.jsx
@@ -1,21 +1,22 @@
-import Book from "./Book"
+import Book from "./Book";
 import "../App.css";
 
-export default function List(){
+export default function List() {
   return (
-    <div id="list-container">
-      <div className="d-flex mx-auto flex-wrap justify-content-around align-items-end h-100 overflow-auto">
-        <Book comments={2}>Libro 1</Book>
-        <Book>Libro 2</Book>
-        <Book comments={6}>Libro 3</Book>
-        <Book comments={2}>Libro 4</Book>
-        <Book>Libro 5</Book>
-        <Book>Libro 6</Book>
-        <Book comments={13}>Libro 7</Book>
-        <Book>Libro 8</Book>
-        <Book comments={1}>Libro 9</Book>
-        <Book>Libro 10</Book>
-      </div>
+    <div
+      id="list-container"
+      className="container-fluid d-flex mx-auto flex-wrap justify-content-between align-items-start p-3 overflow-auto"
+    >
+      <Book comments={2}>Libro 1</Book>
+      <Book>Libro 2</Book>
+      <Book comments={6}>Libro 3</Book>
+      <Book comments={2}>Libro 4</Book>
+      <Book>Libro 5</Book>
+      <Book>Libro 6</Book>
+      <Book comments={13}>Libro 7</Book>
+      <Book>Libro 8</Book>
+      <Book comments={1}>Libro 9</Book>
+      <Book>Libro 10</Book>
     </div>
-  )
+  );
 }
