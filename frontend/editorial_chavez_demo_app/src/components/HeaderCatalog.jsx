0598a6b0bd245f61633060a4bb81d8771164ce80
Yair <yairvelascor05@gmail.com>
2023-07-27 22:35:01 -0600

    37. styles: setting final header height

--- frontend/editorial_chavez_demo_app/src/components/HeaderCatalog.jsx
+++ frontend/editorial_chavez_demo_app/src/components/HeaderCatalog.jsx
@@ -2,13 +2,13 @@ import FilterBy from "./FilterBy";
 
 export default function HeaderCatalog(){
   return (
-    <header className="d-flex flex-md-row justify-content-between bg-white bg-opacity-25 py-3">
-      <nav className="d-flex justify-content-around col-xs-12 col-sm-6 fs-5 text-light" id="nav">
-        <a href="" className="text-light fw-normal">Género 1</a>
-        <a href="" className="text-light fw-normal">Género 2</a>
-        <a href="" className="text-light fw-normal">Género 3</a>
-        <a href="" className="text-light fw-normal">Género 4</a>
-        <a href="" className="text-light fw-normal">Género 5</a>
+    <header className="d-flex flex-md-row justify-content-between bg-white bg-opacity-25 nav-custom-height">
+      <nav className="d-flex justify-content-around col-xs-12 col-sm-6 fs-5 text-light">
+        <a href="" className="text-light fw-normal d-flex align-items-center">Género 1</a>
+        <a href="" className="text-light fw-normal d-flex align-items-center">Género 2</a>
+        <a href="" className="text-light fw-normal d-flex align-items-center">Género 3</a>
+        <a href="" className="text-light fw-normal d-flex align-items-center">Género 4</a>
+        <a href="" className="text-light fw-normal d-flex align-items-center">Género 5</a>
       </nav>
       <FilterBy />
     </header>
