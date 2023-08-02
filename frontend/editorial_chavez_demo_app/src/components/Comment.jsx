a49551a033e019fdfc2fb26054b83413997ce1d2
Miguel Arturo Velasco Ramirez <launchx08269@innovaccion.mx>
2023-08-02 17:39:29 -0600

    47. styles: styling Commment's component's responsiveness and apperance.

--- frontend/editorial_chavez_demo_app/src/components/Comment.jsx
+++ frontend/editorial_chavez_demo_app/src/components/Comment.jsx
@@ -2,9 +2,15 @@ import PropTypes from 'prop-types';
 
 export default function Comment({user, comment}){
   return (
-    <div>
-        <h4>{user}</h4>
-        <p>{comment}</p>
+    <div className='card my-3 py-0 text-start' id="card">
+      <section className="card-body">
+        <h4 className='mt-2 card-title'>{user}</h4>
+        <p className="text-body-secondary"><small>2 hours ago</small></p>
+        <section className="d-flex flex-sm-column flex-md-row align-items-start justify-content-between card-text">
+          <p className="card-text h-25 w-75 text-nowrap text-truncate overflow-hidden">{comment}</p>
+          <button className="bg-transparent text-primary p-0">Ver comentario</button>
+        </section>
+      </section>
     </div>
   )
 }
