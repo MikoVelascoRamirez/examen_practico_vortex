df77452a7d33d7cd33cfba1eb7443218c1323668
Miguel Arturo Velasco Ramirez <launchx08269@innovaccion.mx>
2023-08-02 17:34:48 -0600

    46. styles: styling CommentsPanel's responsiveness and appearance.

--- frontend/editorial_chavez_demo_app/src/components/CommentsPanel.jsx
+++ frontend/editorial_chavez_demo_app/src/components/CommentsPanel.jsx
@@ -2,11 +2,13 @@ import Comment from "./Comment"
 
 const CommentsPanel = () => {
   return (
-    <div className="col-sm col-md-8">
-        <Comment user={"Usuario 1"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
-        <Comment user={"Usuario 2"} comment={"Lorem ipsum, dolor sit amet"}></Comment>
-        <Comment user={"Usuario 3"} comment={"Lorem ipsum, dolor sit amet amet"}></Comment>
-        <Comment user={"Usuario 4"} comment={"Lorem ipsum"}></Comment>
+    <div className="col-sm col-md-8 overflow-auto">
+        <Comment user={"Usuario 1"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
+        <Comment user={"Usuario 2"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
+        <Comment user={"Usuario 3"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
+        <Comment user={"Usuario 4"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
+        <Comment user={"Usuario 5"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
+        <Comment user={"Usuario 6"} comment={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea."}></Comment>
     </div>
   )
 }
