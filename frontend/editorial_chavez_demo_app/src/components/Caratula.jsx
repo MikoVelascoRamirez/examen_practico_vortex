9241c53bbcee77d4572b729c7d7ea516b5871ff9
Miguel Arturo Velasco Ramirez <launchx08269@innovaccion.mx>
2023-08-02 17:32:20 -0600

    45. styles: styling Caratula component

--- frontend/editorial_chavez_demo_app/src/components/Caratula.jsx
+++ frontend/editorial_chavez_demo_app/src/components/Caratula.jsx
@@ -1,8 +1,8 @@
 const Caratula = () => {
   return (
     <div className="col-sm col-md-4">
-        <img src="" alt="" />
-        <p>Libro 1</p>
+        <img src="https://m.media-amazon.com/images/I/71qIRKQx0+L._AC_UF894,1000_QL80_.jpg" alt="book" className="w-auto h-75 mt-3 object-fit-scale rounded"/>
+        <p className="my-3 text-white fs-4">Libro 1</p>
     </div>
   )
 }
